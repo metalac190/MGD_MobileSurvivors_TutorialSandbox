272b4f727f99b89635d883a3f25a7601213b711b
AdamC <achaysmail@gmail.com>
2025-01-09 10:45:50 -0600

    EXP Bar Functional

--- Assets/_Game/Scripts/Gameplay/GameHUDController.cs
+++ Assets/_Game/Scripts/Gameplay/GameHUDController.cs
@@ -4,16 +4,24 @@ using UnityEngine.UIElements;
 public class GameHUDController : MonoBehaviour
 {
     [SerializeField] private GameController _gameController;
+    [SerializeField] private PlayerCharacter _playerCharacter;
 
     private UIDocument _document;
     private VisualElement _loseMenuVisualTree;
     private Button _loseRetryButton;
+
+    private VisualElement _playerHUDVisualTree;
+    private VisualElement _expBarFill;
     private void Awake()
     {
         _document = GetComponent<UIDocument>();
         // get menu sub objects
         _loseMenuVisualTree = _document.rootVisualElement
             .Q("LoseMenuVisualTree");
+        _playerHUDVisualTree = _document.rootVisualElement
+            .Q("PlayerHUDVisualTree");
+        // only search the playerHUDvisual tree, to show we can
+        _expBarFill = _playerHUDVisualTree.Q("ProgressBarFill");
         // assign button callbacks
         _loseRetryButton = _loseMenuVisualTree
             .Q("RetryButton") as Button;
@@ -25,6 +33,7 @@ public class GameHUDController : MonoBehaviour
             <ClickEvent>(OnLoseRetryButtonClick);
 
         _gameController.OnLose.AddListener(DisplayLoseMenu);
+        _playerCharacter.OnEXPGained += SetEXPBarPercent;
     }
     private void OnDisable()
     {
@@ -33,10 +42,12 @@ public class GameHUDController : MonoBehaviour
             <ClickEvent>(OnLoseRetryButtonClick);
 
         _gameController.OnLose.RemoveListener(DisplayLoseMenu);
+        _playerCharacter.OnEXPGained -= SetEXPBarPercent;
     }
     private void Start()
     {
         DisableAllDisplays();
+        //SetEXPBarPercent(30,100);
     }
     private void DisableAllDisplays()
     {
@@ -50,5 +61,13 @@ public class GameHUDController : MonoBehaviour
     {
         _loseMenuVisualTree.style.display = DisplayStyle.None;
         _gameController.ReloadLevel();
+    }
+
+    private void SetEXPBarPercent(float currentLevelEXP, float expForLevelUp)
+    {
+        // formula: 1/max * current is 0-1 value. *100 to convert to %
+        float percentage = ((1/ expForLevelUp) * currentLevelEXP) * 100;
+        // set width as %
+        _expBarFill.style.width = Length.Percent(percentage);
     }
 }
--- Assets/_Game/Scripts/Player/PlayerCharacter.cs
+++ Assets/_Game/Scripts/Player/PlayerCharacter.cs
@@ -1,18 +1,23 @@
 using NUnit.Framework;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerCharacter : MonoBehaviour
 {
+    public event Action OnLeveledUp = delegate { };
+    // OnEXPGained(currentEXP, EXPForNextLevelUp)
+    public event Action<float ,float> OnEXPGained = delegate { };
+
     [SerializeField] private WeaponData _startingWeaponData;
     public List<IUpgrade> ActiveUpgrades { get; private set; } = new List<IUpgrade>();
-
+    // NOTE: we're resetting EXP to 0 every level up
     public float EXP { get; private set; } = 0;
     public int LVL { get; private set; } = 1;
-    // total exp needed for next level up
-    private float _totalEXPNeededForLevelUp = 100;
-    // increase in amount of exp needed once level is gained
-    private float _expNeededIncreaseAmount = 100;
+
+    private float _expForNextLevelUp = 100;
+    // tracking TOTAL exp in case we need it later
+    private float _expTotal = 0;
 
     private GameController _gameController;
     private void Awake()
@@ -35,8 +40,11 @@ public class PlayerCharacter : MonoBehaviour
     public void IncreaseEXP(float amount)
     {
         EXP += amount;
+        Debug.Log("EXP Increase: " + EXP);
+        OnEXPGained.Invoke(EXP, _expForNextLevelUp);
+
         // if we have enough EXP to level up, DING
-        if (EXP >= _totalEXPNeededForLevelUp)
+        if (EXP >= _expForNextLevelUp)
             LevelUp();
     }
 
@@ -45,26 +53,61 @@ public class PlayerCharacter : MonoBehaviour
         // increase level
         LVL++;
         Debug.Log("Level up! " + LVL);
+        // store overflow exp into next level
+        float overflowEXP = EXP - _expForNextLevelUp;
+        // store current EXP in total exp then reset
+        _expTotal += _expForNextLevelUp;
+        // wipe exp to 0, but add any overflow
+        EXP = overflowEXP;
         // increase amount needed for next level
-        _expNeededIncreaseAmount *= 1.5f;
-        // calculate total EXP needed for next level
-        // ex. 100exp, 100 + 150 = 250exp, 250 + 225 = 475exp, etc.
-        _totalEXPNeededForLevelUp += _expNeededIncreaseAmount;
-        // in the future this could be a more complex formula
+        // continues to increase each level up
+        // ex. 100exp, 150exp,  225exp, etc.
+        _expForNextLevelUp *= 1.5f;
 
+        OnEXPGained?.Invoke(EXP, _expForNextLevelUp);
+        OnLeveledUp?.Invoke();
+
+        GetUpgrades();
+    }
+
+    private void GetUpgrades()
+    {
         // get a few options from our Upgrade pool
         List<IUpgrade> upgradeChoices = _gameController.GetUpgradeChoices(3);
-        Debug.Log("upgradeChoices count: " + upgradeChoices.Count);
         foreach (IUpgrade upgrade in upgradeChoices)
         {
             Debug.Log("Choice: " + upgrade.ToString());
         }
         // for simplicity choose one of the options and upgrade automatically
         // in an actual, larger game, we could let the player click a button
-        int randomIndex = Random.Range(0, upgradeChoices.Count);
+        int randomIndex = UnityEngine.Random.Range(0, upgradeChoices.Count);
         IUpgrade newUpgrade = upgradeChoices[randomIndex];
         Debug.Log("Chose: " + newUpgrade.ToString());
         // upgrade ourselves with the chosen upgrade
         newUpgrade.Upgrade(this);
     }
 }
+
+/*
+// increase amount needed for next level
+_expNeededIncreaseAmount *= 1.5f;
+// calculate total EXP needed for next level
+// ex. 100exp, 100 + 150 = 250exp, 250 + 225 = 475exp, etc.
+_totalEXPNeededForLevelUp += _expNeededIncreaseAmount;
+// in the future this could be a more complex formula
+
+// get a few options from our Upgrade pool
+List<IUpgrade> upgradeChoices = _gameController.GetUpgradeChoices(3);
+Debug.Log("upgradeChoices count: " + upgradeChoices.Count);
+foreach (IUpgrade upgrade in upgradeChoices)
+{
+    Debug.Log("Choice: " + upgrade.ToString());
+}
+// for simplicity choose one of the options and upgrade automatically
+// in an actual, larger game, we could let the player click a button
+int randomIndex = Random.Range(0, upgradeChoices.Count);
+IUpgrade newUpgrade = upgradeChoices[randomIndex];
+Debug.Log("Chose: " + newUpgrade.ToString());
+// upgrade ourselves with the chosen upgrade
+newUpgrade.Upgrade(this);
+*/
